323e0acb28cf545d0ba58183d95f5a2d8f23d56e
togauthi <togauthier23@gmail.com>
2025-07-14 11:19:16 +0200

    Should be good but idk

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,13 +1,79 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
+#include <stack>
 
-int main(void)
+int main3()
 {
-	MutantStack<int> s;
-	
-	s.push(4);
-	s.push(5);
-	s.push(6);
-	s.push(7);
+	std::stack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+//[...]
+	mstack.push(0);
+	std::stack<int> s(mstack);
+	return 0;
+}
 
+
+int main2()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
+
+int main()
+{
+	std::cout << "--------------------Main 1-------------------" << std::endl;
+	std::list<int> mstack;
+	mstack.push_back(5);
+	mstack.push_back(17);
+	std::cout << mstack.front() << std::endl;
+	mstack.pop_back();
+	std::cout << mstack.size() << std::endl;
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+//[...]
+	mstack.push_back(0);
+	std::list<int>::iterator it = mstack.begin();
+	std::list<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::list<int> s(mstack);
+	std::cout << "--------------------Main 2-------------------" << std::endl;
+	main2();
+	std::cout << "--------------------Main 3-------------------" << std::endl;
+	main3();
+	return 0;
 }
