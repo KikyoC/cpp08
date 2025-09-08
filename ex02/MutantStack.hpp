a34fe30b369d7e2591e2f1a1df8b89d947e8c06e
togauthi <togauthier23@gmail.com>
2025-09-08 13:14:59 +0200

    ALl iterators

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -19,6 +19,10 @@ class MutantStack: public std::stack<T>
 		~MutantStack(){};
 	
 		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		
 		iterator begin()
 		{
 			return std::stack<T>::c.begin();
@@ -27,4 +31,31 @@ class MutantStack: public std::stack<T>
 		{
 			return std::stack<T>::c.end();
 		}
+		const_iterator begin() const 
+		{
+			return std::stack<T>::c.begin();
+		};
+		const_iterator end() const
+		{
+			return std::stack<T>::c.end();
+		}
+
+		reverse_iterator rbegin()
+		{
+			return std::stack<T>::c.rbegin();
+		};
+		reverse_iterator rend()
+		{
+			return std::stack<T>::c.rend();
+		}
+		const_reverse_iterator rbegin() const 
+		{
+			return std::stack<T>::c.begin();
+		};
+		const_reverse_iterator rend() const
+		{
+			return std::stack<T>::c.end();
+		}
+
+
 };
