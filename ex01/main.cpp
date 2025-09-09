7da6ba9028938ceee1ce1df6993413e094f7401d
togauthi <togauthier23@gmail.com>
2025-09-09 13:46:16 +0200

    Putting right tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,30 +4,32 @@
 #include <iostream>
 #include <vector>
 
-int main(int argc, char **argv)
+int main()
+// int main(int argc, char **argv)
 {
-	Span sp = Span(argc);
+	// Span sp = Span(argc);
 	// Span sp = Span(argc - 2);
-	// sp.addNumber(6);
-	// sp.addNumber(3);
-	// sp.addNumber(17);
-	// sp.addNumber(9);
-	// sp.addNumber(11);
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
 
-	std::vector<int> lst;
-	while (argc > 1)
-	{
-		argc--;
-		lst.push_back(std::atoi(argv[argc]));
-	}
-	try
-	{
-		sp.addNumber(lst);
-	}
-	catch(std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	// std::vector<int> lst;
+	// while (argc > 1)
+	// {
+	// 	argc--;
+	// 	lst.push_back(std::atoi(argv[argc]));
+	// }
+	// try
+	// {
+	// 	sp.addNumber(lst);
+	// }
+	// catch(std::exception &e)
+	// {
+	// 	std::cerr << e.what() << std::endl;
+	// }
 	try
 	{
 		std::cout << sp.shortestSpan() << std::endl;
