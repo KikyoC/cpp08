7b504e5a9eb867f08f7bffd8946470b041192ee3
togauthi <togauthier23@gmail.com>
2025-09-09 12:36:55 +0200

    Added iterators

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -14,6 +14,21 @@ class Span
 
 		void addNumber(int i);
 		void addNumber(std::vector<int> lst);
+		template<typename T>
+		void addNumber(T begin, T end)
+		{
+			int i = 0;
+			for (T it = begin; it != end; it++)
+			{
+				i++;
+			}
+			if (i + this->actual >= this->max)
+				throw SpanFullException();
+			for (T it = begin; it != end; it++)
+			{
+				this->addNumber(*it);
+			}
+		}
 		int shortestSpan() const;
 		int longestSpan() const;
 
